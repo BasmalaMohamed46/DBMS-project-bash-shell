2a277066a17b618ee6150b97e99c8f18def00254
Engy Ahmed Abdelaziz <engya306@gmail.com>
2023-12-20 13:21:44 +0200

    Refactor drop table script

--- TB_dir/dropTable.sh
+++ TB_dir/dropTable.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
-# Execute the list_tb.sh script from the TBprocesses directory
 ../TB_dir/listTable.sh
 
-# Check if x is not equal to 1
-if [ "$x" -ne 1 ]; then
+delete_table() {
     read -p "Enter the table you want to delete: " tbname
 
     if [ -z "$tbname" ]; then
-        echo "Error: Table name cannot be empty. Please enter table name."
+        echo "Error: You cannot enter an empty value. Please enter a valid name."
     elif [ -f "$HOME/db_dir/$1/$tbname" ]; then
-        read -p "Are you sure you want to delete the table '$tbname'? (y/n): " choice
+        read -p "Are you sure you want to delete this table? (y/n): " choice
 
         case $choice in
             [Yy]* )
                 rm "$HOME/db_dir/$1/$tbname"
-                echo "Table $tbname has been deleted."
+                echo "$tbname has been deleted."
                 ;;
             [Nn]* )
-                echo "Operation Canceled. Table '$tbname' was not deleted."
+                echo "Operation Canceled."
                 ;;
             * )
                 echo "Invalid Input. Operation Canceled."
                 ;;
         esac
     else
-        echo "Error: Table $tbname does not exist."
+        echo "Error: The table $tbname does not exist."
     fi
+}
+
+if [ "$x" -ne 1 ]; then
+    delete_table "$1"
 fi
